da9a21580fdf8b683e7e1e8d9447b2420cba78a9
Simon <simon.pierre.fortin@gmail.com>
2018-05-18 10:20:44 -0400

    Update Day21.py

--- Days/19-21_itertools/Day21.py
+++ Days/19-21_itertools/Day21.py
@@ -62,9 +62,13 @@ def get_possible_dict_words(draw):
     """Get all possible words from a draw (list of letters) which are
        valid dictionary words. Use _get_permutations_draw and provided
        dictionary"""
-    pass
+    permutations = [''.join(word).lower()
+                    for word in _get_permutations_draw(draw)]
+    return set(permutations) & set(dictionary)
+
 
 def _get_permutations_draw(draw):
     """Helper to get all permutations of a draw (list of letters), hint:
        use itertools.permutations (order of letters matters)"""
-    pass
+    for i in range(1, 8):
+        yield from list(itertools.permutations(draw, i))
