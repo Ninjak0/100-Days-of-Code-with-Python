943c5a631babe9fd3c9f1e7152af1b9e6fbb5964
Simon <simon.pierre.fortin@gmail.com>
2018-06-13 09:50:47 -0400

    Update Day40.py

--- Days/40-42_json-data/Day40.py
+++ Days/40-42_json-data/Day40.py
@@ -6,6 +6,12 @@ from pprint import pprint
 with open('mount-data.json') as json_data:
     data = json.load(json_data)
 
-for item in data.items():
-    pprint(item)
+is_flying = []
+
+for mount in data["mounts"]["collected"]:
+    if mount["isFlying"]:
+        is_flying.append(mount)
+
+for i in is_flying:
+    pprint(i["name"])
 
